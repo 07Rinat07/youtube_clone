2181415bae44878738d544fce7519f71033be5df
Rinat Sarmuldin <ura07srr@gmail.com>
2023-04-24 21:30:38 +0500

    Getting data by id

---
+++ app/Http/Controllers/ChannelController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Channel;
+
+
+
+
+class ChannelController extends Controller
+{
+    public function index()
+    {
+        return Channel::get();
+    }
+
+    public function show(Channel $channel)
+    {
+        return $channel;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ChannelController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{category}', [CategoryController::class, 'show']);
+
+
+Route::get('/channels', [CategoryController::class, 'index']);
+Route::get('/channels/{channel}', [ChannelController::class, 'index']);
