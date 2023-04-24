ed7b1047c753b8a1a9c45a34876fcbb97c6b4165
Rinat Sarmuldin <ura07srr@gmail.com>
2023-04-24 23:35:41 +0500

    One_to_Many Relationship

---
+++ database/migrations/2023_04_24_171056_create_videos_table.php
@@ -0,0 +1,39 @@
+<?php
+use App\Models\Channel;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+
+
+            $table->foreignIdFor(\App\Models\Channel::class)->constrained();
+//            $table->unsignedBigInteger('channel_id');
+//            $table->foreign('channel_id')->references('id')->on('channels');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ChannelController;
+use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,5 +11,8 @@ Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/{category}', [CategoryController::class, 'show']);
 
 
-Route::get('/channels', [CategoryController::class, 'index']);
-Route::get('/channels/{channel}', [ChannelController::class, 'index']);
+Route::get('/channels', [ChannelController::class, 'index']);
+Route::get('/channels/{channel}', [ChannelController::class, 'show']);
+
+Route::get('/videos', [VideoController::class, 'index']);
+Route::get('/videos/{video}', [VideoController::class, 'show']);
