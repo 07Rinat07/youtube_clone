cef0c15e459f72497b3ceb2a1c8d958342d5fa0d
Rinat Sarmuldin <ura07srr@gmail.com>
2023-04-27 11:11:26 +0500

    Seeders_part_2_random value

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,24 +3,17 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        foreach (range(1, 3) as $i) {
-
-            DB::table('users')->insert([
-                'name' => "User $i",
-                'email' => "user$i@example.com",
-                'password' => "password$i"
-            ]);
-        }
+        $this->call([
+            UserSeeder::class,
+            ChannelSeeder::class,
+            VideoSeeder::class,
+            CategorySeeder::class,
+            CategoryVideoSeeder::class
+        ]);
     }
 }
