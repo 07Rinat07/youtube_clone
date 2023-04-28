0243dbf093cec3e2e4bedeaf30e2588b3bbb898c
Rinat Sarmuldin <ura07srr@gmail.com>
2023-04-28 13:48:43 +0500

    Limit and order by constraints

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -1,15 +1,19 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Enums\Period;
 use App\Models\Video;
+
 class VideoController extends Controller
 {
     public function index()
     {
         $period = Period::tryFrom(request('period'));
-
         return Video::fromPeriod($period)
             ->search(request('query'))
+            ->limit(request('limit'))
+            ->orderBy(request('sort', 'created_at'), request('order', 'asc'))
             ->get();
     }
 
