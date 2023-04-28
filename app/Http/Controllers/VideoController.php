8de25a0337c466c624ae62199811c0478150be2a
Rinat Sarmuldin <ura07srr@gmail.com>
2023-04-28 13:36:50 +0500

    Search constrain(local scope)

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Enums\Period;
 use App\Models\Video;
-
 class VideoController extends Controller
 {
     public function index()
@@ -12,8 +9,7 @@ class VideoController extends Controller
         $period = Period::tryFrom(request('period'));
 
         return Video::fromPeriod($period)
-            ->where('title', 'like', '%' . request('query') . '%')
-            ->orWhere('description', 'like', '%' . request('query') . '%')
+            ->search(request('query'))
             ->get();
     }
 
--- app/Models/Video.php
+++ app/Models/Video.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace App\Models;
-
 use App\Enums\Period;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class Video extends Model
 {
     use HasFactory;
@@ -20,5 +17,13 @@ class Video extends Model
     public function scopeFromPeriod($query, ?Period $period)
     {
         return $period ? $query->where('created_at', '>=', $period->date()) : $query;
+    }
+
+    public function scopeSearch($query, ?string $text)
+    {
+        return $query->where(function ($query) use ($text) {
+            $query->where('title', 'like', "%$text%")
+                ->orWhere('description', 'like', "%$text%");
+        });
     }
 }
