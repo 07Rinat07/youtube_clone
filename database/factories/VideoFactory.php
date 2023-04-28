d06d742695cfdeca38cb71e7b148e31f4402b147
Rinat Sarmuldin <ura07srr@gmail.com>
2023-04-28 11:26:10 +0500

    Reading video by time period_&& create and fresh Build Docker_Sail_Project

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -1,15 +1,24 @@
 <?php
-
 namespace Database\Factories;
-
 use App\Models\Channel;
+use App\Models\Video;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
 class VideoFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Video::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
     public function definition()
     {
-        $createdAt = $this->faker->dateTimeThisYear();
+       // $createdAt = $this->faker->dateTimeThisYear();
         // $createdAt = $this->faker->dateTimeThisMonth();
         // $createdAt = $this->faker->dayOfWeek();
         // $createdAt = $this->faker->dateTimeBetween('-2 months', '+1 week');
@@ -20,6 +29,7 @@ class VideoFactory extends Factory
         // $createdAt = $this->faker->dateTimeBetween('-1 week');
         // $createdAt = $this->faker->dateTimeBetween('-1 day');
         // $createdAt = $this->faker->dateTimeBetween('-1 hour');
+        $createdAt = $this->createdAt();
 
         return [
             'title' => ucfirst($this->faker->words(mt_rand(1, 2), true)),
@@ -27,5 +37,12 @@ class VideoFactory extends Factory
             'created_at' => $createdAt,
             'updated_at' => $createdAt,
         ];
+    }
+
+    private function createdAt()
+    {
+        $period = $this->faker->randomElement(['year', 'month', 'week', 'day', 'hour']);
+
+        return $this->faker->dateTimeBetween("-1 $period");
     }
 }
