2bb2c9008ed944872d29d37807742f323e72590c
Rinat Sarmuldin <ura07srr@gmail.com>
2023-04-29 22:36:32 +0500

    Filter, sort and paginate categories.index request

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -14,5 +14,8 @@ class Category extends Model
         return $this->belongsToMany(Video::class);
     }
 
-
+    public function scopeSearch($query, ?string $name)
+    {
+        return $query->where('name', 'like', "%$name%");
+    }
 }
