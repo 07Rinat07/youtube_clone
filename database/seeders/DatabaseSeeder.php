056c028de8be97cdc94b0ba0859a7e7be69ec209
Rinat Sarmuldin <ura07srr@gmail.com>
2023-04-30 00:38:04 +0500

    Create playlist resource

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,19 +1,22 @@
 <?php
-
 namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
     public function run()
     {
         $this->call([
             UserSeeder::class,
             ChannelSeeder::class,
+            PlaylistSeeder::class,
             VideoSeeder::class,
             CategorySeeder::class,
-            CategoryVideoSeeder::class
+            CategoryVideoSeeder::class,
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ChannelController;
+use App\Http\Controllers\PlaylistController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Route;
@@ -12,13 +12,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{user}', [UserController::class, 'show']);
 
-
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/{category}', [CategoryController::class, 'show']);
 
-
 Route::get('/channels', [ChannelController::class, 'index']);
 Route::get('/channels/{channel}', [ChannelController::class, 'show']);
+
+Route::get('/playlists', [PlaylistController::class, 'index']);
+Route::get('/playlists/{playlist}', [PlaylistController::class, 'show']);
 
 Route::get('/videos', [VideoController::class, 'index']);
 Route::get('/videos/{video}', [VideoController::class, 'show']);
