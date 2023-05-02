cd7f6d1825d528ec3ee5fb66944b16be9b8c7c86
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-02 22:06:44 +0500

    Check nested relationships_49_less

--- app/Traits/WithRelationships.php
+++ app/Traits/WithRelationships.php
@@ -1,16 +1,23 @@
 <?php
-namespace App\Traits;
 
+namespace App\Traits;
 trait WithRelationships
 {
     public function scopeWithRelationships($query, array|string $relationships)
     {
         $validRelationships = collect($relationships)
-            ->map(fn (string $relationships): array => explode('.', $relationships))
-            ->filter(fn (array $relationships): bool => in_array($relationships[0], static::$relationships))
-            ->map(fn (array $relationships): string => implode('.', $relationships))
-            ->all();
+            ->map(fn(string $relationships): array => explode('.', $relationships))
+            ->filter(function (array $relationships) {
+                return collect($relationships)->reduce(function ($model, $relationship) {
+                    if ($model && method_exists($model, $relationship) && in_array($relationship, $model::$relationships)) {
+                        return $model->$relationship()->getRelated();
+                    }
 
+                    return null;
+                }, new static);
+            })
+            ->map(fn(array $relationships): string => implode('.', $relationships))
+            ->all();
         return $query->with($validRelationships);
     }
 }
