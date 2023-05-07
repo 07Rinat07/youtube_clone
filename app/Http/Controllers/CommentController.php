866da42bdf0da112ba49c62282ca788788d5a0bd
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-07 16:56:59 +0500

    Update comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -1,22 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Comment;
 use Illuminate\Http\Request;
-
 class CommentController extends Controller
 {
     public function index()
     {
         return Comment::with('parent', 'user', 'video')->get();
     }
-
     public function show(Comment $comment)
     {
         return $comment;
     }
-
     public function store(Request $request)
     {
         $attributes = $request->validate([
@@ -24,13 +20,20 @@ class CommentController extends Controller
             'parent_id' => 'exists:comments,id',
             'video_id' => 'required_without:parent_id|exists:videos,id'
         ]);
-
         $attributes['user_id'] = $request->user()->id;
-
         if ($request->parent_id) {
             $attributes['video_id'] = Comment::find($request->parent_id)->video_id;
         }
 
         return Comment::create($attributes);
+    }
+
+    public function update(Comment $comment, Request $request)
+    {
+        $attributes = $request->validate([
+            'text' => 'required|string',
+        ]);
+
+        $comment->fill($attributes)->save();
     }
 }
