c0be7ce028969159d30a779e349fc9e0d36f63ed
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-10 14:46:58 +0500

    Refactoring comments logic_LESS_62

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -1,25 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
-namespace App\Http\Controllers;
-
 use App\Models\Comment;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
-
 class CommentController extends Controller
 {
     public function index()
     {
         return Comment::with('parent', 'user', 'video')->get();
     }
-
     public function show(Comment $comment)
     {
         return $comment;
     }
-
     public function store(Request $request)
     {
         $attributes = $request->validate([
@@ -27,27 +21,29 @@ class CommentController extends Controller
             'parent_id' => 'exists:comments,id',
             'video_id' => 'required_without:parent_id|exists:videos,id',
         ]);
-        $attributes['user_id'] = $request->user()->id;
-        if ($request->parent_id) {
-            $attributes['video_id'] = Comment::find($request->parent_id)->video_id;
-        }
+
         return Comment::create($attributes);
     }
 
     public function update(Comment $comment, Request $request)
     {
-        throw_if($request->user()->isNot($comment->user), AuthorizationException::class);
+        $this->checkPermissions($comment, $request);
+
         $attributes = $request->validate([
             'text' => 'required|string',
         ]);
-
         $comment->fill($attributes)->save();
     }
 
     public function destroy(Comment $comment, Request $request)
     {
-        throw_if($request->user()->isNot($comment->user), AuthorizationException::class);
+        $this->checkPermissions($comment, $request);
 
         $comment->delete();
+    }
+
+    private function checkPermissions(Comment $comment, Request $request)
+    {
+        throw_if($request->user()->isNot($comment->user), AuthorizationException::class);
     }
 }
--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -1,14 +1,22 @@
 <?php
+
 namespace Database\Factories;
+
 use App\Models\Comment;
 use App\Models\User;
 use App\Models\Video;
 use Illuminate\Database\Eloquent\Factories\Factory;
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
  */
 class CommentFactory extends Factory
 {
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
     public function definition()
     {
         return [
@@ -16,5 +24,14 @@ class CommentFactory extends Factory
             'user_id' => User::inRandomOrder()->first() ?: User::factory(),
             'video_id' => Video::inRandomOrder()->first() ?: Video::factory(),
         ];
+    }
+
+    public function reply()
+    {
+        return $this->state(function () {
+            return [
+                'parent_id' => Comment::inRandomOrder()->first() ?: Comment::factory(),
+            ];
+        });
     }
 }
