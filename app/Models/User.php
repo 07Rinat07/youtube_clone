54e3df8c717f95b98ecf0805cb65d124f44546ef
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-17 12:08:26 +0500

    Password mutator_75_les

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,24 @@
 namespace App\Models;
 
 use App\Traits\WithRelationships;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
+
 {
     use HasApiTokens, HasFactory, Notifiable, WithRelationships;
 
     protected static $relationships = ['channel', 'comments'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'name',
         'email',
@@ -35,6 +43,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected function password(): Attribute
+    {
+        return Attribute::make(
+            set: fn(string $password) => Hash::make($password)
+        );
+    }
 
     public function channel()
     {
