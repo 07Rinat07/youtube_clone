5c19ea64103e52110f43b45540740c702bb21b40
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-19 14:28:04 +0500

    Send email verification link_81_les

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
@@ -16,9 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest');
 Route::delete('/delete-account', [RegisteredUserController::class, 'destroy'])->middleware('auth');
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])->middleware('guest');
 Route::delete('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth');
+
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->middleware('auth');
+
+Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)->middleware('auth')->name('verification.verify');
