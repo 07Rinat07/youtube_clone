86c59d9a987ca06a95f9d4fdf9eadc416e8936e1
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-22 17:17:29 +0500

    Customize password reset email_88_les

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\ResetPasswordNotification;
 use App\Traits\WithRelationships;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Casts\Attribute;
@@ -14,7 +15,9 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, WithRelationships;
+
     protected static $relationships = ['channel', 'comments'];
+
     /**
      * The attributes that are mass assignable.
      *
@@ -25,6 +28,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'email',
         'password',
     ];
+
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -34,6 +38,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'password',
         'remember_token',
     ];
+
     /**
      * The attributes that should be cast to native types.
      *
@@ -42,29 +47,44 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
     protected static function booted()
     {
         static::deleting(fn (User $user) => $user->tokens()->delete());
     }
+
     protected function password(): Attribute
     {
         return Attribute::make(
             set: fn (string $password) => Hash::make($password)
         );
     }
+
     public function channel()
     {
         return $this->hasOne(Channel::class);
     }
+
     public function comments()
     {
         return $this->hasMany(Comment::class);
     }
+
     public function scopeSearch($query, ?string $text)
     {
         return $query->where(function ($query) use ($text) {
             $query->where('name', 'like', "%$text%")
                 ->orWhere('email', 'like', "%$text%");
         });
+    }
+
+    public function sendPasswordResetNotification($token): void
+    {
+        $url = url(route('password.reset', [
+            'token' => $token,
+            'email' => $this->getEmailForPasswordReset(),
+        ], false));
+
+        $this->notify(new ResetPasswordNotification($url));
     }
 }
